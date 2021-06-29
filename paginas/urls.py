d90e32d0a1573214cdc142ba13cc9062ebf9972d
Francielxyz <ruanvha15@gmail.com>
2021-06-28 21:22:46 -0300

    Configuração das Páginas

--- paginas/urls.py
+++ paginas/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path 
-from .views import IndexView # . indica que esta pegando algo dentro deste diretório (pasta paginas)
+from .views import ModeloView, IndexView, PedidoView # . indica que esta pegando algo dentro deste diretório (pasta paginas)
 
 urlpatterns = [
     #path('endereço'/', MinhaView.as_view(), name='nome-da-url')
     path('', IndexView.as_view(), name='inicio'),
+    path('pedido/', PedidoView.as_view(), name='pedido'),
 ]
--- paginas/views.py
+++ paginas/views.py
@@ -1,5 +1,11 @@
 from django.views.generic import TemplateView
 
 #Feito herana de TemplateView como se fosse o extends de java
+class ModeloView(TemplateView):
+    template_name = "paginas/modelo.html"
+
 class IndexView(TemplateView):
     template_name = "paginas/index.html"
+
+class PedidoView(TemplateView):
+    template_name = "paginas/pedido.html"
